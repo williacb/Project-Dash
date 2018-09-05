3866ea9dfcb90c3b4d607eaf9804b14ca0957412
williacb <42593332+williacb@users.noreply.github.com>
2018-09-05 11:21:58 -0400

    js app

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#dashboard',
+  data: {
+  "name" : "Tapestry",
+  "short_description": "Build a visualization layer for the project dashboard",
+  "start_date" : "2018-07-01",
+  "target_date" : "2018-11-03",
+  "budget" : "4950000",
+  "spent" : "3456700",
+  "projected_spend": "4740500",
+  "weekly_effort_target": 400
+}
+})
